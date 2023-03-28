aa3a7124fb2b6cf092d637dd86f097a768afe39c
shotttik <shota.akhlouri@gmail.com>
2023-03-28 20:38:39 +0400

    started working on search component, created input element in commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SvgIconComponent } from './svg-icon/svg-icon.component';
+import { SearchComponent } from './components/search/search.component';
+import { InputComponent } from './elements/input/input.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,8 +16,10 @@ import { SvgIconComponent } from './svg-icon/svg-icon.component';
     HeaderComponent,
     FooterComponent,
     SvgIconComponent,
+    SearchComponent,
+    InputComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })

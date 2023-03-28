4ac0cc7594da8732ddb51440f1c885c8fe64870d
shotttik <shota.akhlouri@gmail.com>
2023-03-28 19:43:07 +0400

    footer finished

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  infoMenu = [
+    'ჩვენ შესახებ',
+    'პარტნიორები',
+    'ექიმებისთვის',
+    'კლინიკებისთვის',
+    'აფთიაქებისთვის',
+  ];
+  forPatientsMenu = ['ექიმები', 'კლინიკები', 'ანოტაციები', 'ბლოგი', 'მედია'];
 
+  contactMenu = [
+    'სამუშაო დღეები : ორშ - პარ',
+    'სამუშაო დრო : 9:00 - 17:00',
+    'იაკობ ნიკოლაძე №10',
+    '032 2 100 100',
+  ];
+  followMenu = ['Facebook', 'Instagram', 'Youtube'];
+  constructor() {}
+  ngOnInit(): void {}
 }

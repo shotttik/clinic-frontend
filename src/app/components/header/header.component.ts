3387db972e3c137149cf7e2c7b98ba607b66181b
shotttik <shota.akhlouri@gmail.com>
2023-03-31 16:33:47 +0400

    added open modal styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AuthorizationComponent } from 'src/app/pages/authorization/authorization.component';
-
+import { ElementRef, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  authDialogOpened = false;
+
   navItems = [
     'ექიმები',
     'კლინიკები',
@@ -21,10 +23,15 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {}
 
   openDialog() {
+    this.authDialogOpened = true;
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = false; //cannot close if user click outside the dialog
     dialogConfig.autoFocus = true;
-    this.dialog.open(AuthorizationComponent, dialogConfig);
+    const dialogRef = this.dialog.open(AuthorizationComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .subscribe((data) => (this.authDialogOpened = false));
   }
 }

522db7f412e821c9d3432ff22089c9601c7110dc
shotttik <shota.akhlouri@gmail.com>
2023-04-14 14:36:22 +0400

    added select element and customized

---
+++ src/app/elements/select/select.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css'],
+})
+export class SelectComponent {
+  @Output() selectValueEvent = new EventEmitter<string>();
+  @Input() placeholder = '';
+  options = [
+    { id: 1, category: 'ორთმოლოგი' },
+    { id: 2, category: 'ყბაყური' },
+  ];
+  selectedOption: string = '';
+  valueChanged(v: string) {
+    this.selectValueEvent.emit(v);
+  }
+}
